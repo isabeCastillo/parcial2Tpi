5b6059510a40c126ed2bbcd8395e50778c29a6c0
isabeCastillo <cp21026@ues.edu.sv>
2025-10-02 11:23:12 -0600

    feat:agregando contenido a index.php y autoloader

--- mvc_patron/autoloader.php
+++ mvc_patron/autoloader.php
@@ -1,10 +1,12 @@
-<?php
-   spl_autoload_register(function($clase){
-       $ruta = "../".str_replace("\\","/",$clase).".php";
-       if(file_exists($ruta)) {
-           require_once $ruta;
-       } else {
-           die("No se pudo cargar la ruta $ruta");
-       }
-   });
+<?php 
+spl_autoload_register(function($clase){
+    $ruta ="../".str_replace("\\","/",$clase).".php";
+
+    if(file_exists($ruta)){
+        require_once($ruta);
+
+    }else{
+        die("No se pudo cargar la ruta $ruta");
+    }
+});
 ?>
